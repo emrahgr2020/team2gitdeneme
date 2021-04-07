f9a17dc101a4b1ccb9fb185ac57e6c305b934296
Emrullah Celebi <78147110+emrahgr2020@users.noreply.github.com>
2021-04-07 18:39:03 +0300

    Update Deneme.java

--- src/team2gitdeneme/Deneme.java
+++ src/team2gitdeneme/Deneme.java
@@ -3,7 +3,7 @@ package team2gitdeneme;
 public class Deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Github dan merhabalar");
 
 	}
 
