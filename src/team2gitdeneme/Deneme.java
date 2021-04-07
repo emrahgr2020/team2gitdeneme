a8ec1d204263fe10c0b6944842d3fef4c4734240
Emrullah Celebi <78147110+emrahgr2020@users.noreply.github.com>
2021-04-07 18:43:39 +0300

    Update Deneme.java

--- src/team2gitdeneme/Deneme.java
+++ src/team2gitdeneme/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("Github dan merhabalar");
 		System.out.println("Git den merhabalar");
+		System.out.println("Github dan merhabalar2");
 	}
 
 }
